1a7421241908b514df7f34323716b003caa0792e
Denis <kartavenkodenis0@gmail.com>
2016-07-04 13:54:23 +0300

    Split_And_Remove_ShortWords

---
+++ src/RemoveShortWordsAndSplit.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+vector<wstring> RemoveShortWordsAndSplit(wstring str, int len);
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include "SplitSentences.h"
+#include "RemoveShortWordsAndSplit.h"
 #include <vector>
 #include <string>
 using namespace std;
 int main(int argc, char** argv){
     setlocale(LC_ALL, "");
-    wstring str = L"ЙЙЙЙЙЙЙЙ. йцвйцвйцв. йцу.йц.в.йцв.йцв.йц.в. цйцвцЦЦВЦЙвйцвцйв.";
-    vector<wstring> v = SplitSentence(str);
+    wstring str = L"Ыфвфывфыв фыюыв& фыв? фывююыфвыф. Ффвыфвыфвыфв! фывыфвыфв. Фывфывфыв.";
+    vector<wstring> v = RemoveShortWordsAndSplit(str, 5);
     for (auto i = v.begin(); i != v.end(); ++i)
-    	wcout << *i << L"\n";
+    	wcout << *i << L" ";
 
     return 0;
 }

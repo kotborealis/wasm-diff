6b8f29248b319d297d4ee4cf23386bad3aa2f3f9
kotborealis <kotborealis@gmail.com>
2016-07-04 15:08:18 +0300

    Fixed ReadFileToWString function
    Closing file after reading

--- src/ReadFile.cpp
+++ src/ReadFile.cpp
@@ -7,8 +7,11 @@
 std::wstring ReadFileToWString(std::string filename){
     std::wifstream stream(filename.c_str());
     stream.imbue(std::locale(""));
+
     std::wstring str;
     for(std::wstring c; stream >> c;)
-       str+=c+L'\n';
+        str+=c+L'\n';
+
+    stream.close();
     return str;
 }

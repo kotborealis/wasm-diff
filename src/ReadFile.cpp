09fc6e426c987b748cdef01c91348cb6fa3d20f2
kotborealis <kotborealis@gmail.com>
2016-07-04 13:31:38 +0300

    Added ReadFileToWString function to read utf8 files

---
+++ src/ReadFile.cpp
@@ -0,0 +1,14 @@
+#include "ReadFile.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+std::wstring ReadFileToWString(std::string filename){
+    std::wifstream stream(filename.c_str());
+    stream.imbue(std::locale(""));
+    std::wstring str;
+    for(std::wstring c; stream >> c;)
+       str+=c+L'\n';
+    return str;
+}

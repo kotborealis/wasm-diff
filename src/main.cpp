b5b0c9a5f05efab3b2b79849c7093f5d1a9574b7
kotborealis <kotborealis@gmail.com>
2016-07-05 22:39:11 +0300

    JSON output

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,46 @@
 
 using namespace std;
 int main(int argc, char** argv){
+    if(argc<3){
+        return 0;
+    }
     setlocale(LC_ALL, "");
-    std::string o = ReadFileToString("./tests/test1a");
-    std::string n = ReadFileToString("./tests/test1b");
+    std::string o = ReadFileToString(argv[1]);
+    std::string n = ReadFileToString(argv[2]);
     std::vector<DIFF_INFO*> diff = diffString(o,n);
 
-    for(auto it = diff.begin();it!=diff.end();it++){
-        if((*it)->type==DIFF_EQUAL)
-            std::cout<<(*it)->text;
-        else if((*it)->type==DIFF_REMOVE)
-            if((*it)->text!="\n" && (*it)->text!=" ")
-                std::cout<<'-'<<(*it)->text;
-            else
-                std::cout<<(*it)->text;
-        else if((*it)->type==DIFF_INSERT)
-            if((*it)->text!="\n" && (*it)->text!=" ")
-                std::cout<<'+'<<(*it)->text;
-            else
-                std::cout<<(*it)->text;
+    std::cout<<"[";
+    int it;
+    for(it = 0; it < diff.size()-1; it++){
+        DIFF_TYPE type = diff[it]->type;
+        std::string str = diff[it]->text;
+        if(str=="\n")str="\\n";
+        switch(type){
+            case DIFF_EQUAL:
+                std::cout<<"[0,\""<<str<<"\"],";
+                break;
+            case DIFF_INSERT:
+                std::cout<<"[1,\""<<str<<"\"],";
+                break;
+            case DIFF_REMOVE:
+                std::cout<<"[-1,\""<<str<<"\"],";
+                break;
+        }
     }
+    DIFF_TYPE type = diff[it]->type;
+    std::string str = diff[it]->text;
+    if(str=="\n")str="\\n";
+    switch(type){
+        case DIFF_EQUAL:
+            std::cout<<"[0,\""<<str<<"\"]";
+            break;
+        case DIFF_INSERT:
+            std::cout<<"[1,\""<<str<<"\"]";
+            break;
+        case DIFF_REMOVE:
+            std::cout<<"[-1,\""<<str<<"\"]";
+            break;
+    }
+    std::cout<<"]";
     return 0;
 }

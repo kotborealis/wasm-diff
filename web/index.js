77a347cd8e421fe2b7ff460b65c0105877451b41
kotborealis <kotborealis@gmail.com>
2016-07-07 00:38:37 +0300

    Updated web interface

--- web/index.js
+++ web/index.js
@@ -5,14 +5,21 @@ const diffString = diffStringLib();
 
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
 app.get('/diff/:line_a/:line_b',(req,res)=>{
 	const text1 = req.params.line_a;
 	const text2 = req.params.line_b;
+	res.json(diffString(text1,text2));
+});
+app.post('/diff',(req,res)=>{
+	const text1 = req.body.text1;
+	const text2 = req.body.text2;
 	res.json(diffString(text1,text2));
 });
 

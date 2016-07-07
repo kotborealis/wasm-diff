d0259c0810c698c628011e198ddbe5ab92423754
kotborealis <kotborealis@gmail.com>
2016-07-07 23:11:07 +0300

    "optimizations"?

--- web/public/diff.js
+++ web/public/diff.js
@@ -1,5 +1,6 @@
 const diff = {};
 
+/*main*/
 diff.diffFiles = (files)=>{
 	fetch("http://"+location.host+"/diff/",{
 		method: 'post',
@@ -29,96 +30,101 @@ diff.show_sideBySide = (_diff)=>{
 	let _id = 0;
 	_diff = _diff.filter(e=>e.text.length>0);
 
-	let buff_left = "";
-	let buff_right = "";
+	const buff_left = [];
+	const buff_right = [];
 
 	n_diff = diff.splitDiff(_diff);
 
 	n_diff.forEach(line=>{
 		let changed = false;
-		let buff_line_left = "";
-		let buff_line_right = "";
+		const buff_line_left = [];
+		const buff_line_right = [];
 		line.forEach(d=>{
 			const type = d.type;
-			const text = d.text.replace(/</g,'&lt;').replace(/>/g,'&gt;');
+			const text = d.text;
 
 			if(type!==0)
 				changed=true;
 
 			switch(type){
 				case 0:
-					buff_line_left  += diff.getHtml.diff__equal(text);
-					buff_line_right += diff.getHtml.diff__equal(text);
+					buff_line_left.push(diff.el.info(text,''));
+					buff_line_right.push(diff.el.info(text,''));
 					break;
 				case 1:
-					buff_line_right += diff.getHtml.diff__insert(text);
+					buff_line_right.push(diff.el.info(text,'diff__insert'));
 					break;
 				case -1:
-					buff_line_left  += diff.getHtml.diff__remove(text);
+					buff_line_left.push(diff.el.info(text,'diff__remove'));
 					break;
 			}
 		});
 
-		if(changed){
-			buff_line_left = diff.getHtml.diff__old(buff_line_left,_id);
-			buff_line_right = diff.getHtml.diff__new(buff_line_right,_id);
-		}
-		else{
-			buff_line_left = diff.getHtml.diff__eql_l(buff_line_left,_id);
-			buff_line_right = diff.getHtml.diff__eql_r(buff_line_right,_id);
-		}
-		_id++;
+		let _;
+		if(changed)
+			_ = diff.el.highlight(buff_line_left,buff_line_right,'diff__old','diff__new',_id);
+		else
+			_ = diff.el.highlight(buff_line_left,buff_line_right,'diff__eql','diff__eql',_id);
 
-		buff_left += buff_line_left;
-		buff_right += buff_line_right;
+		buff_left.push(_[0]);
+		buff_right.push(_[1]);
+
+		_id++;
 	});
-	diff.setInnerHTML(buff_left,0);
-	diff.setInnerHTML(buff_right,1);
+	const diff_l = document.getElementsByClassName("diff")[0];
+	const diff_r = document.getElementsByClassName("diff")[1];
+	
+	buff_left.forEach(e=>diff_l.appendChild(e));
+	buff_right.forEach(e=>diff_r.appendChild(e));
 };
 
-diff.setInnerHTML = (buff,i)=>
-	document.getElementsByClassName("diff")[i].innerHTML = buff;
+/*elements*/
+diff.el = {};
 
-diff.getHtml = {};
+diff.el.info  = (text,class_) =>{
+	const e = document.createElement('span');
+	e.textContent = text;
+	e.className = class_;
+	return e;
+};
 
-diff.getHtml.diff__equal  = (e) => e;
-diff.getHtml.diff__insert = (e) => "<span class='diff__insert'>"+e+"</span>";
-diff.getHtml.diff__remove = (e) => "<span class='diff__remove'>"+e+"</span>";
+diff.el.highlight = (elements_a,elements_b,class_a,class_b,id) =>{
+	console.log(elements_a,elements_b,class_a,class_b,id);
+	const el = document.createElement('span');
+	el.className = class_a;
+	el.onmouseleave = diff.highlight_hide;
+	elements_a.forEach(e=>el.appendChild(e));
 
-// Large strings are faster then concatenating small pieces
-diff.getHtml.diff__eql_l = (e,id) =>
-	"<span class='diff__eql' onmouseover=\"diff.highlight('el"+id+"','er"+id+"')\" onmouseleave=\"diff.highlight_hide();\" id='el"+id+"'>"+e+"</span>";
-diff.getHtml.diff__eql_r = (e,id) => 
-	"<span class='diff__eql' onmouseover=\"diff.highlight('er"+id+"','el"+id+"')\" onmouseleave=\"diff.highlight_hide();\" id='er"+id+"'>"+e+"</span>";
+	const er = document.createElement('span');
+	er.className = class_b;
+	er.onmouseleave = diff.highlight_hide;
+	elements_b.forEach(e=>er.appendChild(e));
+	
+	el.onmouseover = ()=>diff.highlight(el,er);
+	er.onmouseover = ()=>diff.highlight(er,el);
 
-diff.getHtml.diff__old = (e,id) => 
-	"<span class='diff__old' onmouseover=\"diff.highlight('o"+id+"','n"+id+"')\" onmouseleave=\"diff.highlight_hide();\" id='o"+id+"'>"+e+"</span>";
+	return [el,er];
+}
 
-diff.getHtml.diff__new = (e,id) => 
-	"<span class='diff__new' onmouseover=\"diff.highlight('n"+id+"','o"+id+"')\" onmouseleave=\"diff.highlight_hide();\" id='n"+id+"'>"+e+"</span>";
-
+/*highlight*/
 diff.cur_highlighted;
 
-diff.highlight = (id1,id2)=>{
-	const e1 = document.getElementById(id1);
-	const e2 = document.getElementById(id2);
+diff.highlight = (e1,e2)=>{
 	diff.cur_highlighted = [e1,e2];
 
 	diff.cur_highlighted[0].classList.add('diff__highlight');
 
-	if(diff.cur_highlighted[1]!==null){
-		diff.cur_highlighted[1].classList.add('diff__highlight');
-		tooltip.innerHTML = diff.cur_highlighted[1].innerHTML;
-		tooltip.showAtElement(diff.cur_highlighted[0]);
-	}
+	diff.cur_highlighted[1].classList.add('diff__highlight');
+	tooltip.innerHTML = diff.cur_highlighted[1].innerHTML;
+	tooltip.showAtElement(diff.cur_highlighted[0]);
 };
 
 diff.highlight_hide = ()=>{
 	tooltip.hide();
 	diff.cur_highlighted[0].classList.remove('diff__highlight');
-	if(diff.cur_highlighted[1]!==null)
-		diff.cur_highlighted[1].classList.remove('diff__highlight');
+	diff.cur_highlighted[1].classList.remove('diff__highlight');
 };
+
 /*file select*/
 let diffFiles = null;
 let diffFilesCount = 0;

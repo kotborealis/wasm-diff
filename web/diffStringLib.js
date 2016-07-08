11c0c15054ea448c18c39f79692f5a538a5d86af
kotborealis <kotborealis@gmail.com>
2016-07-08 11:31:27 +0300

    Changed server to be async

--- web/diffStringLib.js
+++ web/diffStringLib.js
@@ -17,19 +17,20 @@ module.exports = function (){
 		'__diffString': [diff_info_array,['string','string']]
 	});
 
-	return function(text1, text2){
-		const _ = libdiff.__diffString(text1,text2);
+	return function(text1, text2, callback){
+		const _ = libdiff.__diffString.async(text1,text2,(err,res)=>{
+			if(err) throw err;
+			const _ = res;
+			_.length=1;
+			_.length = diff_info(_[0]).type+1;
 
-		_.length=1;
-		_.length = diff_info(_[0]).type+1;
-
-		const diff=[];
-		for(let i=1; i<_.length;i++)
-			diff.push({
-				type: diff_info(_[i]).type,
-				text: diff_info(_[i]).text
-			});
-
-		return diff;
+			const diff=[];
+			for(let i=1; i<_.length;i++)
+				diff.push({
+					type: diff_info(_[i]).type,
+					text: diff_info(_[i]).text
+				});
+			callback(diff);
+		});
 	}
 }
--- web/index.js
+++ web/index.js
@@ -15,12 +15,12 @@ app.use(express.static('public'));
 app.get('/diff/:line_a/:line_b',(req,res)=>{
 	const text1 = req.params.line_a;
 	const text2 = req.params.line_b;
-	res.json(diffString(text1,text2));
+	diffString(text1,text2,(diff)=>res.json(diff));
 });
 app.post('/diff',upload.array('diff_files[]',2),(req,res)=>{
 	const text1 = req.files[0].buffer.toString();
 	const text2 = req.files[1].buffer.toString();
-	res.json(diffString(text1,text2));
+	diffString(text1,text2,(diff)=>res.json(diff));
 });
 
 app.listen(8019);
